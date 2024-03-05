e061b99366ca7063a129c3ab8a8108b3890229d1
Bradythebest27 <brady.setser@yahoo.com>
2024-03-04 23:39:17 -0500

    remove nonfunction stuff

--- script.js
+++ script.js
@@ -29,30 +29,3 @@ window.addEventListener('scroll', function() {
         }
     });
 });
-
-// Revolving Language Icons
-window.onload = function() {
-    const container = document.querySelector('.language-container');
-    const languageIcons = document.querySelectorAll('.language-icon');
-    const containerWidth = container.offsetWidth;
-    const containerHeight = container.offsetHeight;
-    const iconSize = 100;
-    const centerX = containerWidth / 2;
-    const centerY = containerHeight / 2;
-    const radius = containerWidth / 2 - iconSize / 2;
-    const speed = 0.01; // Adjust the speed as desired
-
-    function animate() {
-        let angle = 0;
-        languageIcons.forEach(icon => {
-            const x = centerX + radius * Math.cos(angle);
-            const y = centerY + radius * Math.sin(angle);
-            icon.style.left = `${x - iconSize / 2}px`;
-            icon.style.top = `${y - iconSize / 2}px`;
-            angle += (2 * Math.PI) / languageIcons.length;
-        });
-        requestAnimationFrame(animate);
-    }
-
-    animate();
-};
